cda13004da3a5a36fe608bcbf3b02ccf272a01a2
root <root@DigitalOcean.Debian.MinetestServer>
2021-01-15 19:24:37 +0000

    Automatic update per 20210115

---
+++ routine.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x # Echo on
+# Reset pwd
+cd /var/www/html
+# Configure path variables
+minetestmapper="/root/programming/minetestmapper/minetestmapper"
+worldpath="/var/games/minetest-server/.minetest/worlds/world_dragon/"
+symbolpath="./maps/latest.png"
+# Get current date in required format
+printf -v date '%(%Y%m%d)T' -1
+filename="${date}.png"
+filepath="./maps/${filename}"
+# Compile new map
+${minetestmapper} -i ${worldpath} -o ${filepath}
+# Update symbolic link
+ln -sf ${filepath} ${symbolpath}
+# Update git repository
+git add .
+git commit -a -m "Automatic update per ${date}"
+git push
---
+++ test.sh
@@ -0,0 +1,2 @@
+printf -v date '%(%Y%m%d)T.png' -1
+echo $date
